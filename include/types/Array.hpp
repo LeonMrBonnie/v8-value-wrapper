53c991d9b2e08995ed143ec1ca624d5781afc7f7
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-02-12 22:30:38 +0100

    Fix Array and Object

--- include/types/Array.hpp
+++ include/types/Array.hpp
@@ -3,67 +3,38 @@
 #include "v8/include/v8.h"
 
 #include "../Value.hpp"
+#include "../Generic.hpp"
 
 namespace JSWrapper
 {
-    class Array : public Value<std::vector<Generic>, v8::Array>
+    v8::Local<v8::Array> Value<std::vector<Generic>, v8::Array>::ToJSValue()
     {
-        v8::Local<v8::Array> ToJSValue()
+        v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
+        v8::Local<v8::Array> arr = v8::Array::New(isolate, (int)cppValue.size());
+        for(size_t i = 0; i < cppValue.size(); i++)
         {
-            v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
-            v8::Local<v8::Array> arr = v8::Array::New(isolate, (int)cppValue.size());
-            for(size_t i = 0; i < cppValue.size(); i++)
-            {
-                arr->Set(ctx, i, cppValue.at(i).GetValue());
-            }
-            return arr;
+            arr->Set(ctx, i, cppValue.at(i).GetValue());
         }
-        std::vector<Generic> ToCppValue(std::vector<Generic>&& defaultVal)
-        {
-            if(!GetHandle()->IsArray()) return defaultVal;
+        return arr;
+    }
+    std::vector<Generic> Value<std::vector<Generic>, v8::Array>::ToCppValue(std::vector<Generic>&& defaultVal)
+    {
+        if(!GetHandle()->IsArray()) return defaultVal;
 
-            v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
-            uint32_t size = GetHandle().As<v8::Array>()->Length();
-            std::vector<Generic> vector;
-            vector.reserve(size);
-            for(uint32_t i = 0; i < size; i++)
-            {
-                v8::MaybeLocal<v8::Value> maybeVal = GetHandle().As<v8::Array>()->Get(ctx, i);
-                v8::Local<v8::Value> val;
-                if(!maybeVal.ToLocal(&val)) return defaultVal;
-                vector.push_back(val);
-            }
-            return vector;
-        }
-
-    public:
-        Generic At(uint32_t index)
+        v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
+        uint32_t size = GetHandle().As<v8::Array>()->Length();
+        std::vector<Generic> vector;
+        vector.reserve(size);
+        for(uint32_t i = 0; i < size; i++)
         {
-            if(hasCppValue) return cppValue.at(index);
-            else if(hasJSValue)
-            {
-                v8::MaybeLocal<v8::Value> maybeValue = GetHandle().As<v8::Array>()->Get(isolate->GetEnteredOrMicrotaskContext(), index);
-                v8::Local<v8::Value> value;
-                if(!maybeValue.ToLocal(&value)) return Generic{};
-                return value;
-            }
-            else
-                return Generic{};
+            v8::MaybeLocal<v8::Value> maybeVal = GetHandle().As<v8::Array>()->Get(ctx, i);
+            v8::Local<v8::Value> val;
+            if(!maybeVal.ToLocal(&val)) return defaultVal;
+            vector.push_back(val);
         }
+        return vector;
+    }
 
-        Generic operator[](int index)
-        {
-            return At(index);
-        }
-
-        size_t GetSize()
-        {
-            if(hasCppValue) return cppValue.size();
-            else if(hasJSValue)
-                return GetHandle().As<v8::Array>()->Length();
-            else
-                return 0;
-        }
-    };
+    using Array = Value<std::vector<Generic>, v8::Array>;
 
 };  // namespace JSWrapper
--- include/types/Object.hpp
+++ include/types/Object.hpp
@@ -10,57 +10,33 @@
 
 namespace JSWrapper
 {
-    class Object : public Value<std::unordered_map<std::string, Generic>, v8::Object>
+    v8::Local<v8::Object> Value<std::unordered_map<std::string, Generic>, v8::Object>::ToJSValue()
     {
-        v8::Local<v8::Object> ToJSValue()
+        v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
+        v8::Local<v8::Object> obj = v8::Object::New(isolate);
+        for(auto& [key, value] : cppValue)
         {
-            v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
-            v8::Local<v8::Object> obj = v8::Object::New(isolate);
-            for(auto& [key, value] : cppValue)
-            {
-                obj->Set(ctx, String{ key }.JSValue(), value.GetValue());
-            }
-            return obj;
+            obj->Set(ctx, String{ key }.JSValue(), value.GetValue());
         }
-        std::unordered_map<std::string, Generic> ToCppValue(std::unordered_map<std::string, Generic>&& defaultVal)
-        {
-            if(!GetHandle()->IsObject()) return defaultVal;
-            v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
-            std::vector<Generic> keys = Array{ GetHandle().As<v8::Object>()->GetOwnPropertyNames(ctx) }.CppValue();
-            std::unordered_map<std::string, Generic> map;
-            for(auto& key : keys)
-            {
-                v8::MaybeLocal<v8::Value> maybeVal = GetHandle().As<v8::Object>()->Get(ctx, key.GetValue());
-                v8::Local<v8::Value> val;
-                if(!maybeVal.ToLocal(&val)) return defaultVal;
-                map.insert({ String{ key }.CppValue(), val });
-            }
-            return map;
-        }
+        return obj;
+    }
+    std::unordered_map<std::string, Generic> Value<std::unordered_map<std::string, Generic>, v8::Object>::ToCppValue(std::unordered_map<std::string, Generic>&& defaultVal)
+    {
+        if(!GetHandle()->IsObject()) return defaultVal;
 
-    public:
-        Generic At(const std::string& key)
+        v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
+        std::vector<Generic> keys = Array{ GetHandle().As<v8::Object>()->GetOwnPropertyNames(ctx) }.CppValue();
+        std::unordered_map<std::string, Generic> map;
+        for(auto& key : keys)
         {
-            if(hasCppValue) return cppValue.at(key);
-            else if(hasJSValue)
-            {
-                v8::MaybeLocal<v8::Value> maybeValue = GetHandle().As<v8::Array>()->Get(isolate->GetEnteredOrMicrotaskContext(), String{ key }.JSValue());
-                v8::Local<v8::Value> value;
-                if(!maybeValue.ToLocal(&value)) return Generic{};
-                return value;
-            }
-            else
-                return Generic{};
+            v8::MaybeLocal<v8::Value> maybeVal = GetHandle().As<v8::Object>()->Get(ctx, key.GetValue());
+            v8::Local<v8::Value> val;
+            if(!maybeVal.ToLocal(&val)) return defaultVal;
+            map.insert({ String{ key }.CppValue(), val });
         }
+        return map;
+    }
 
-        Generic operator[](const std::string& key)
-        {
-            return At(key);
-        }
-        Generic operator[](const char* key)
-        {
-            return At(key);
-        }
-    };
+    using Object = Value<std::unordered_map<std::string, Generic>, v8::Object>;
 
 };  // namespace JSWrapper
