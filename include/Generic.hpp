72b4ce8bf3679c0b1db245301df56026bced7860
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-02-12 21:13:39 +0100

    Add Generic class

---
+++ include/Generic.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "v8/include/v8.h"
+
+namespace JSWrapper
+{
+    enum class GenericType : uint8_t
+    {
+        INVALID,
+        NUMBER,
+        BIGINT,
+        BOOL,
+        STRING,
+        ARRAY,
+        OBJECT
+    };
+
+    class Generic
+    {
+        v8::Local<v8::Value> value;
+        GenericType type;
+
+    public:
+        Generic() : type(GenericType::INVALID) {}
+        Generic(v8::Local<v8::Value> _value) : value(_value), type(GetGenericType(_value)) {}
+
+        GenericType GetType()
+        {
+            return type;
+        }
+        v8::Local<v8::Value> GetValue()
+        {
+            return value;
+        }
+
+        static GenericType GetGenericType(v8::Local<v8::Value> value)
+        {
+            if(value->IsNumber()) return GenericType::NUMBER;
+            if(value->IsBigInt()) return GenericType::BIGINT;
+            if(value->IsBoolean()) return GenericType::BOOL;
+            if(value->IsString()) return GenericType::STRING;
+            if(value->IsArray()) return GenericType::ARRAY;
+            if(value->IsObject()) return GenericType::OBJECT;
+            return GenericType::INVALID;
+        }
+    };
+};  // namespace JSWrapper
